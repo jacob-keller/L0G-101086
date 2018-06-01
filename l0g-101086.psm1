0515838bce1a9f62bb2a95fa91b128b006277981
Jacob Keller <jacob.keller@gmail.com>
2018-05-31 22:56:32 -0700

    extract configuration validation to separate function
    In preparation for handling a new configuration format, extract the valid
    fields for a v1 config object to a global variable. Use a separate
    function to perform the configuration validation.
    
    This paves the way to add a v2ConfigurationFields variable which contains
    the valid configuration for a v2 config file.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- l0g-101086.psm1
+++ l0g-101086.psm1
@@ -81,6 +81,164 @@ Function ConvertTo-UnixDate {
 }
 
 <#
+ .Description
+  Configuration fields which are valid for a v1 configuration file. Anything
+  not listed here will be excluded from the generated $config object. If one
+  of the fields has an incorrect type, configuration will fail to be validated.
+
+  If $path is set, then the configuration will allow exchanging %UserProfile%
+  for the current $env:USERPROFILE value
+#>
+$v1ConfigurationFields =
+@{
+    "gw2raidar_token"=@{
+        type=[string]
+    };
+    "dps_report_token"=@{
+        type=[string]
+    };
+    "discord_webhook"=@{
+        type=[string]
+    };
+    "guild_thumbnail"=@{
+        type=[string]
+    };
+    "gw2raidar_tag_glob"=@{
+        type=[string]
+    };
+    "guild_text"=@{
+        type=[string]
+    };
+    "discord_map"=@{
+        type=[PSCustomObject]
+    };
+    "emoji_map"=@{
+        type=[PSCustomObject]
+    };
+    "restsharp_path"=@{
+        type=[string]
+        path=$true
+    };
+    "discord_json_data"=@{
+        type=[string]
+        path=$true
+    };
+    "last_format_file"=@{
+        type=[string]
+        path=$true
+    };
+    "last_upload_file"=@{
+        type=[string]
+        path=$true
+    };
+    "extra_upload_data"=@{
+        type=[string]
+        path=$true
+    };
+    "gw2raidar_start_map"=@{
+        type=[string]
+        path=$true
+    };
+    "simple_arc_parse_path"=@{
+        type=[string]
+        path=$true
+    };
+    "custom_tags_script"=@{
+        type=[string]
+        path=$true
+    };
+    "arcdps_logs"=@{
+        type=[string]
+        path=$true
+    };
+    "upload_log_file"=@{
+        type=[string]
+        path=$true
+    };
+    "guildwars2_path"=@{
+        type=[string]
+        path=$true
+    };
+    "dll_backup_path"=@{
+        type=[string]
+        path=$true
+    };
+    "publish_fractals"=@{
+        type=[bool]
+    };
+    "debug_mode"=@{
+        type=[bool]
+    };
+    "config_version"=@{
+        type=[int]
+    }
+}
+
+<#
+ .Synopsis
+  Validate a configuration object to make sure it has correct fields
+
+ .Description
+  Take a $config object, and verify that it has valid parameters with the expected
+  information and types. Return the $config object on success (with updated path names)
+  Return $null if the $config object is not valid.
+
+ .Parameter config
+  The configuration object to validate
+#>
+Function Validate-Configuration {
+    [CmdletBinding()]
+    param([PSCustomObject]$config,
+          [int]$version)
+
+    if ($version -eq 1) {
+        $configurationFields = $v1ConfigurationFields
+    } else {
+        Read-Host -Prompt "BUG: configuration validation does not support version ${version}. Press enter to exit"
+        exit
+    }
+
+    # For now, allow an empty config_version
+    if (-not $config.PSObject.Properties.Match("config_version")) {
+        Write-Host "Configuration file is missing config_version field. This will be required in a future update. Please set it to the value '1'"
+    }
+
+    # Make sure the config_version is set to 1. This should only be bumped if
+    # the expected configuration names change. New fields should not cause a
+    # bump in this version, but only removal or change of fields.
+    #
+    # Scripts should be resilient against new parameters not being configured.
+    if ($config.config_version -ne $version) {
+        Read-Host -Prompt "This script only knows how to understand config_version=${version}. Press enter to exit"
+        return
+    }
+
+    # Select only the known properties, ignoring unknown properties
+    $config = $config | Select-Object -Property @($ConfigurationFields.Keys)
+
+    $invalid = $false
+    foreach ($field in $ConfigurationFields.Keys) {
+        # Make sure that the field has the expected type
+        if ($config."${field}" -isnot $ConfigurationFields[$field].type) {
+            Write-Host "${field} has an unexpected type [$($config."${field}".GetType().name)]"
+            $invalid = $true
+        }
+
+        # Handle %UserProfile% in path fields
+        if ($ConfigurationFields[$field].path) {
+            $config."${field}" = $config."${field}".replace("%UserProfile%", $env:USERPROFILE)
+        }
+    }
+
+    if ($invalid) {
+        Read-Host -Prompt "Configuration file has invalid parameters. Press enter to exit"
+        return
+    }
+
+    return $config
+}
+
+<#
  .Synopsis
   Load the configuration file and return a configuration object
 
@@ -111,125 +269,8 @@ Function Load-Configuration {
         return
     }
 
-    # For now, allow an empty config_version
-    if (-not $config.PSObject.Properties.Match("config_version")) {
-        Write-Host "Configuration file is missing config_version field. This will be required in a future update. Please set it to the value '1'"
-    }
-
-    # Make sure the config_version is set to 1. This should only be bumped if
-    # the expected configuration names change. New fields should not cause a
-    # bump in this version, but only removal or change of fields.
-    #
-    # Scripts should be resilient against new parameters not being configured.
-    if ($config.config_version -ne 1) {
-        Read-Host -Prompt "This script only knows how to understand config_version=1. Press enter to exit"
-        return
-    }
-
-    # List of current configuration values
-    $ConfigurationFields = @{
-        "gw2raidar_token"=@{
-            type=[string]
-        };
-        "dps_report_token"=@{
-            type=[string]
-        };
-        "discord_webhook"=@{
-            type=[string]
-        };
-        "guild_thumbnail"=@{
-            type=[string]
-        };
-        "gw2raidar_tag_glob"=@{
-            type=[string]
-        };
-        "guild_text"=@{
-            type=[string]
-        };
-        "discord_map"=@{
-            type=[PSCustomObject]
-        };
-        "emoji_map"=@{
-            type=[PSCustomObject]
-        };
-        "restsharp_path"=@{
-            type=[string]
-            path=$true
-        };
-        "discord_json_data"=@{
-            type=[string]
-            path=$true
-        };
-        "last_format_file"=@{
-            type=[string]
-            path=$true
-        };
-        "last_upload_file"=@{
-            type=[string]
-            path=$true
-        };
-        "extra_upload_data"=@{
-            type=[string]
-            path=$true
-        };
-        "gw2raidar_start_map"=@{
-            type=[string]
-            path=$true
-        };
-        "simple_arc_parse_path"=@{
-            type=[string]
-            path=$true
-        };
-        "custom_tags_script"=@{
-            type=[string]
-            path=$true
-        };
-        "arcdps_logs"=@{
-            type=[string]
-            path=$true
-        };
-        "upload_log_file"=@{
-            type=[string]
-            path=$true
-        };
-        "guildwars2_path"=@{
-            type=[string]
-            path=$true
-        };
-        "dll_backup_path"=@{
-            type=[string]
-            path=$true
-        };
-        "publish_fractals"=@{
-            type=[bool]
-        };
-        "debug_mode"=@{
-            type=[bool]
-        };
-        "config_version"=@{
-            type=[int]
-        }
-    }
-
-    # Select only the known properties, ignoring unknown properties
-    $config = $config | Select-Object -Property @($ConfigurationFields.Keys)
-
-    $invalid = $false
-    foreach ($field in $ConfigurationFields.Keys) {
-        # Make sure that the field has the expected type
-        if ($config."${field}" -isnot $ConfigurationFields[$field].type) {
-            Write-Host "${field} has an unexpected type [$($config."${field}".GetType().name)]"
-            $invalid = $true
-        }
-
-        # Handle %UserProfile% in path fields
-        if ($ConfigurationFields[$field].path) {
-            $config."${field}" = $config."${field}".replace("%UserProfile%", $env:USERPROFILE)
-        }
-    }
-
-    if ($invalid) {
-        Read-Host -Prompt "Configuration file has invalid parameters. Press enter to exit"
+    $config = (Validate-Configuration $config 1)
+    if (-not $config) {
         return
     }
 
