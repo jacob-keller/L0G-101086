jacob-keller/L0G-101086

commit 8a14b9e4d0049f634df6bd0539a0ada81d1da268

upload-logs: warn about outdated ArcDPS version
gw2raidar is very picky about what encounters it accepts, and will only
publish encounters which are created with the most recent ArcDPS version.
This can confuse users who happen to forget to update, or are attempting
to upload previous encounters. Add a warning about this in the log
message, by comparing the most recent ArcDPS release with the version in
each file.

We still attempt to upload even if the encounter is out of date, in the
off chance that we are incorrect, and gw2raidar will accept the encounter
after all.

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

upload-logs.ps1
@@ -121,6 +121,10 @@ function Log-Output ($string) {
     }
 }
 
+# Determine the most recent release of ArcDPS
+$arcdps_headers = (Invoke-WebRequest -UseBasicParsing -Uri https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum).Headers
+$arcdps_release_date = (Get-Date -Date ($arcdps_headers['Last-Modified'])).Date
+
 # If we have a last upload file, we want to limit our scan to all files since
 # the last time that we uploaded.
 #
@@ -171,6 +175,27 @@ ForEach($f in $files) {
         # Parse the evtc header file and get the encounter name and id
         $evtc_header_data = (& $simple_arc_parse header "${evtc}")
         $evtc_header = ($evtc_header_data.Split([Environment]::NewLine))
+
+        # Determine the ArcDPS release date of this encounter
+        try {
+            $evtc_arcpds_version = [DateTime]::ParseExact($evtc_header[0], 'EVTCyyyyMMdd', $null)
+
+            # gw2raidar is extremely picky about uploading new encounters, and will generally
+            # only parse the most recent release of ArcDPS. Warn the user if the version of
+            # for this encounter is out of date. We'll still try to upload to gw2raidar, but
+            # at least the user will be aware that the links may not be generated.
+            if ($evtc_arcpds_version -lt $arcdps_release_date) {
+                Log-Output "It appears that ${name} was recorded using an outdated ArcDPS version released on $(Get-Date -Format "MMM d, yyyy" $evtc_arcdps_version)"
+                Log-Output "The most recent ArcDPS version was releasted on $(Get-Date -Format "MMM d, yyyy" $arcdps_release_date)"
+                Log-Output "gw2raidar is unlikely to accept this encounter, so you might not see a link for it in the formatted encounters list"
+                Log-Output "It is recommended that you update ArcDPS to avoid this issue."
+            }
+        } catch {
+            Log-Output "$PSItem"
+            Log-Output "Unable to determine the ArcDPS version used to record ${name}"
+        }
+
+
 
         # Determine the guild to associate with this encounter
         $guild = Determine-Guild $config.guilds $players $evtc_header[2]

commit 87a78e1d6820055d8551d55004b5c0d5986e51d6

upload-logs: fix typo in evtc_arcdps_version variable
When introducing the outdated ArcDPS version check, I accidentally spelled
the variable evtc_arcpds_version for some instances. This caused the one
correct spelling to result in a $null pointer, which caused the try block
to execute inadvertently.

Fix these to use the correctly named evtc_arcdps_version. This will allow
the warning about outdateda arcpds version to be printed.

Fixes: 8a14b9e ("upload-logs: warn about outdated ArcDPS version", 2018-06-06)
Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

upload-logs.ps1
@@ -187,13 +187,13 @@ ForEach($f in $files) {
 
         # Determine the ArcDPS release date of this encounter
         try {
-            $evtc_arcpds_version = [DateTime]::ParseExact($evtc_header[0], 'EVTCyyyyMMdd', $null)
+            $evtc_arcdps_version = [DateTime]::ParseExact($evtc_header[0], 'EVTCyyyyMMdd', $null)
 
             # gw2raidar is extremely picky about uploading new encounters, and will generally
             # only parse the most recent release of ArcDPS. Warn the user if the version of
             # for this encounter is out of date. We'll still try to upload to gw2raidar, but
             # at least the user will be aware that the links may not be generated.
-            if ($evtc_arcpds_version -lt $arcdps_release_date) {
+            if ($evtc_arcdps_version -lt $arcdps_release_date) {
                 Log-Output "It appears that ${name} was recorded using an outdated ArcDPS version released on $(Get-Date -Format "MMM d, yyyy" $evtc_arcdps_version)"
                 Log-Output "The most recent ArcDPS version was releasted on $(Get-Date -Format "MMM d, yyyy" $arcdps_release_date)"
                 Log-Output "gw2raidar is unlikely to accept this encounter, so you might not see a link for it in the formatted encounters list"