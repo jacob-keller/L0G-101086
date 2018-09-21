da546043954d9453be44c858714e2cb10dd58c8d
Jacob Keller <jacob.keller@gmail.com>
2018-09-20 21:47:31 -0700

    format-encounters: add experimental support for wing6 bosses
    Add the wing6 bosses to the boss list. Because gw2raidar doesn't list the
    wing6 boss ids in it's areas API, we need to hack these into the array
    manually.
    
    You likely will not save evtc files for these new encounters without using
    the experimental development build of ArcDPS. Additionally, currently the
    encounters do not match properly.
    
    This is still experimental and is likely to change in the near future.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- upload-logs.ps1
+++ upload-logs.ps1
@@ -105,7 +105,7 @@ if (-not $extra_upload_data) {
 }
 
 # Make sure that simpleArcParse version matches our expectation
-$expected_simple_arc_version = "v0.12"
+$expected_simple_arc_version = "v0.13"
 $simple_arc_version = (& $simple_arc_parse version)
 if ($simple_arc_version -eq "") {
     Write-Host "Unable to determine the version of simpleArcParse"
