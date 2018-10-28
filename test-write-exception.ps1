2dd123091213188f9c4d916c01388cd296642af4
Jacob Keller <jacob.keller@gmail.com>
2018-10-27 23:12:36 -0700

    l0g-101068: self-sign powershell scripts
    Use a self-generated private key to self-sign the powershell scripts. This
    isn't extremely useful, since the key is not generated by a public key
    authority, but it is better than leaving the scripts unsigned.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

---
+++ test-write-exception.ps1
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2018 Jacob Keller. All rights reserved.
+
+# Terminate on all errors...
+$ErrorActionPreference = "Stop"
+
+# Load the shared module
+Import-Module -Force -DisableNameChecking (Join-Path -Path $PSScriptRoot -ChildPath l0g-101086.psm1)
+
+try {
+    throw "an exception occurred"
+} catch {
+    Write-Exception $_
+}
