80eaa22abfbfc5afd2435d4414c9148c1690429b
Jacob Keller <jacob.e.keller@intel.com>
2019-03-26 00:35:26 -0700

    catch errors when building up compount exception objects
    When writing out an exception, an attempt is made to combine the data of
    sub exceptions as much as possible. However, sometimes this can fail,
    such as when a parameter already exists. Avoid throwing an exception
    inside the Write-Exception function by catching this error
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>

--- l0g-101086.psm1
+++ l0g-101086.psm1
@@ -1001,7 +1001,13 @@ Function Write-Exception {
     # Combine the exception and invocation parameters together into a single list
     $info = $e.InvocationInfo | Select *
     $e.Exception | Get-Member -MemberType Property | ForEach-Object {
-        $info | Add-Member -MemberType NoteProperty -Name $_.Name -Value ( $e.Exception | Select-Object -ExpandProperty $_.Name )
+        try {
+            $info | Add-Member -MemberType NoteProperty -Name $_.Name -Value ( $e.Exception | Select-Object -ExpandProperty $_.Name )
+        } catch {
+            # Catch errors when adding new properties, to avoid accidentally
+            # throwing an exception here when trying to write out the
+            # exception details
+        }
     }
 
     Write-Error ( $info | Format-List -Force | Out-String) -ErrorAction Continue
