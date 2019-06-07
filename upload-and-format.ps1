36add34d83343762e4a01ceb61b5bb6d8d54028a
Jacob Keller <jacob.e.keller@intel.com>
2019-06-06 18:07:13 -0700

    add an upload-and-format script
    This script just calls out to both the upload-logs.ps1 script and the
    format-encounters.ps1 script.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>

---
+++ upload-and-format.ps1
@@ -0,0 +1,48 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2019 Jacob Keller. All rights reserved.
+# vim: et:ts=4:sw=4
+
+# Terminate on all errors...
+$ErrorActionPreference = "Stop"
+
+# Load the shared module
+Import-Module -Force -DisableNameChecking (Join-Path -Path $PSScriptRoot -ChildPath l0g-101086.psm1)
+
+Log-Output "Uploading logs..."
+& .\upload-logs.ps1
+
+Log-Output "Formatting and publishing to discord..."
+
+& .\format-encounters.ps1
+# SIG # Begin signature block
+# MIIFhQYJKoZIhvcNAQcCoIIFdjCCBXICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU2RJXQYgGzgPUbdKaojVqen7z
+# 2rKgggMYMIIDFDCCAfygAwIBAgIQLNFTiNzlwrtPtvlsLl9i3DANBgkqhkiG9w0B
+# AQsFADAiMSAwHgYDVQQDDBdMMEctMTAxMDg2IENvZGUgU2lnbmluZzAeFw0xOTA1
+# MTEwNjIxMjNaFw0yMDA1MTEwNjQxMjNaMCIxIDAeBgNVBAMMF0wwRy0xMDEwODYg
+# Q29kZSBTaWduaW5nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz8yX
+# +U/I8mljHGNNqj3Yu5m41ibtP7vXqhoFF16AWFMVI26sCFknvKO95h8ByCyyrSJy
+# KouRR+bLwYg/a8ElqBA3r3nvnefWzFuj19lYoChautae6n1Yg80/V5XuY9tXjXRs
+# LLA+rDCJBDTtku0Y7ahk5KOGwnqxY520BKt8A/MOD3mQnUtxZ88C7Otr4jr+2k9k
+# CM7oMD1jJsmFpZxaDinsPiYobs/NRJ4iAlTN+NgwmHrj+Tgpln5GHhCpncUbZ530
+# ODbndMwYkW3T7JECjxZYLg4B6CzXFw+SDewIq0svCnIBa+NQYHzNvdwJU5xlTdG+
+# n3RSRT0N1UgrUnQ/OQIDAQABo0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAww
+# CgYIKwYBBQUHAwMwHQYDVR0OBBYEFL0r6+kPYUrlpu8rKWwyrLWuc3zNMA0GCSqG
+# SIb3DQEBCwUAA4IBAQADr9YRypADuVVOiwbrKYT5GLBa+1wbDHdC9YRWf+kGtKYC
+# K4RsIgCngakR6MmksUhNgYRBN6pD4qTOgkUEfxmpLSjTyEYkcslF/Y5sBwiVRqS2
+# p38Ay5byGfRRb/KbjndE7vEM0DJg3XWbayiiARhe6Af0FXgg0F7n5AblnZrUuE1x
+# 62I5N3lSsH8xjF8BcvtSh+jhDypIBAjyNMwzPvO8hGMoqrpNY5IjvBWrHPGzrm90
+# Jju/ucR3d14J6MwoCxcisupXdRhkIE9c4MiW67tf019h4TBnUNzW8DWyoprKAIRV
+# qjO6XExzBeHTPOH8olN/oYaOmqUC9c9MEolbolhRMYIB1zCCAdMCAQEwNjAiMSAw
+# HgYDVQQDDBdMMEctMTAxMDg2IENvZGUgU2lnbmluZwIQLNFTiNzlwrtPtvlsLl9i
+# 3DAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG
+# 9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIB
+# FTAjBgkqhkiG9w0BCQQxFgQUNUl7HaFECexBrw035oPxLdglmBwwDQYJKoZIhvcN
+# AQEBBQAEggEABSBMWTqqjIwSr5yUkcXBULTZINYlyAZDSWjPsk4hZL6wiqysy+N4
+# kTm31/I+odahCg4p+Ji0wVsHIL5WozasAmCb54GKf32o2Ez8KYB4J6FybAEsHW1M
+# DB3qhEY4mmSsbRL0SnwAk0G7wlTe48s1ipVvFUCHzSbpSKQ/SIKu5X/J+Uz+nngn
+# iIhe1rWK7JTK+PTaeCWeR05TGQ6ZUDq4q5lld+HzCio45mtzaNknYU4o4HHw2FHD
+# 0oCTzstWuOClNjENWu15i5cJShM5xNmfVtJ3vaW2NmbyYAK4CuitS5Gk4fTRRZFl
+# pg3aVC7tjawms6zw6br0/b3xy8R2ZoASSA==
+# SIG # End signature block
